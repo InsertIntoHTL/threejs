d3913d38c7d3c6cc710d1facc93809da6c2804c5
InsertIntoHTL <5125@htl.rennweg.at>
2020-03-13 12:50:03 +0100

    Last push

--- js/main.js
+++ js/main.js
@@ -4,68 +4,51 @@ import {GLTFLoader} from "../three.js-master/examples/jsm/loaders/GLTFLoader.js"
 
 if (WEBGL.isWebGLAvailable()) {
 
-    let scene, camera, renderer, cube;
+    let scene, camera, renderer, cube, light;
 
     function init() {
-
         scene = new THREE.Scene();
         scene.background = new THREE.Color(0xffffff);
 
-        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        light = new THREE.AmbientLight(0xffffff);
+        scene.add(light);
+
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100000);
+        camera.position.set(150, 180, 280);
 
         renderer = new THREE.WebGLRenderer({antialias: true});
         renderer.setSize(window.innerWidth, window.innerHeight);
 
         document.body.appendChild(renderer.domElement);
 
-
         let controls = new OrbitControls(camera, renderer.domElement);
-
-        /*
-        let geometry = new THREE.BoxGeometry();
-        let material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-        cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );*/
 
         var loader = new GLTFLoader();
 
         loader.load(
-            // resource URL
-            'models/eevee/eevee.gltf',
-            // called when the resource is loaded
+            'models/eevee/eevee.glb',
             function ( gltf ) {
-
                 let object = gltf.scene.children[0];
-                object.scale.set(0.1,0.1,0.1);
-                object.position.set(0, -10, -0.75);
-
+                object.scale.set(1,1,1);
+                object.position.set(0, 0, 0);
                 scene.add( object );
-
                 renderer.render(scene, camera);
-
             },
-            // called while loading is progressing
             function ( xhr ) {
-
                 console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
             },
-            // called when loading has errors
             function ( error ) {
-
                 console.log( 'An error happened' );
                 console.log(error)
-
             }
         );
-
         camera.position.z = 5;
-
-        window.addEventListener('resize', onWindowResize);
-
+        /*window.addEventListener('resize', onWindowResize);*/
     }
 
-    function animate() {
+    init();
+
+    /*function animate() {
         requestAnimationFrame(animate);
 
 //		update();
@@ -84,8 +67,7 @@ if (WEBGL.isWebGLAvailable()) {
         camera.updateProjectionMatrix();
     }
 
-    init();
-    animate();
+    animate();*/
 
 } else {
 
