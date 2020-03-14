d3b10e54c0de03411399c3cee4a73e03700213a8
Sandra <sandra.boez@gmail.com>
2020-03-14 12:07:42 +0100

    update to see the model in the scene

--- js/main.js
+++ js/main.js
@@ -1,79 +1,70 @@
-import {OrbitControls} from '../three.js-master/examples/jsm/controls/OrbitControls.js';
-import {WEBGL} from "../three.js-master/examples/jsm/WebGL.js";
-import {GLTFLoader} from "../three.js-master/examples/jsm/loaders/GLTFLoader.js";
+/*
+    I didn't use import by habit, anyway it not works for me ^^
+    So I think it was the big problem because your code is good.
+    And I just delete camera.position.x = 5 
 
-if (WEBGL.isWebGLAvailable()) {
+    You'll have an error in the console :
+    "THREE.Loader: Handlers.get() has been removed. Use LoadingManager.getHandler() instead."
+    But I can't help you, I didn't have the motivation to switch to the new function for my own code
+*/
 
-    let scene, camera, renderer, cube, light;
+let renderer, scene, camera;
 
-    function init() {
-        scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xffffff);
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
 
-        light = new THREE.AmbientLight(0xffffff);
-        scene.add(light);
+    let light = new THREE.AmbientLight(0xffffff); 
+    scene.add(light);
 
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.set(150, 180, 280);
 
-        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100000);
-        camera.position.set(150, 180, 280);
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-        renderer = new THREE.WebGLRenderer({antialias: true});
-        renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-        document.body.appendChild(renderer.domElement);
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.update();
 
-        let controls = new OrbitControls(camera, renderer.domElement);
+    const loader = new THREE.GLTFLoader();
 
-        var loader = new GLTFLoader();
-
-        loader.load(
-            'models/windmill/windmill.glb',
-            function ( gltf ) {
-                let object = gltf.scene.children[0];
-                object.scale.set(1,1,1);
-                object.position.set(0, 0, 0);
-                scene.add( object );
-                renderer.render(scene, camera);
-            },
-            function ( xhr ) {
-                console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-            },
-            function ( error ) {
-                console.log( 'An error happened' );
-                console.log(error)
-            }
-        );
-        camera.position.z = 5;
-        /*window.addEventListener('resize', onWindowResize);*/
-    }
-
-    init();
-
-    /*function animate() {
-        requestAnimationFrame(animate);
-
-//		update();
-
-        renderer.render(scene, camera);
-    }
-
-    function update() {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-    }
-
-    function onWindowResize() {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    }
-
-    animate();*/
-
-} else {
-
-    let warning = WEBGL.getWebGLErrorMessage();
-    document.getElementById('warning').appendChild(warning);
-
+/*
+    Your models are black, I think you have not included images texture in the .glb 
+    So I gived you a model with a texture.
+    You can find out about textures in forums, it's easy to do with Blender ;-) 
+*/
+    loader.load(
+        'models/empire_state_building.glb',
+        function ( gltf ) {
+            let object = gltf.scene;
+            object.scale.set(10,10,10);
+            object.position.set(0, 0, 0);
+            scene.add( object );
+        },
+        function ( xhr ) {
+            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        },
+        function ( error ) {
+            console.log( 'An error happened' );
+            console.log(error)
+        }
+    );
+    window.addEventListener('resize', onWindowResize);
 }
 
+/* renderer here otherwise OrbitControls doesn't work */
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+}
+
+init();
+animate();
